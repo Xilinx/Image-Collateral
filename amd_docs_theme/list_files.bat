088f3c5f216893b29ea9675a0f8b292dab701dec
Ryan Vergel <ryan.vergel@amd.com>
2025-04-10 13:23:26 -0600

    Delete list_files.bat

--- amd_docs_theme/list_files.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Generating file list, please wait...
-for /r %%f in (*) do @echo %%f >> file_list.txt
-echo File list has been generated successfully as "file_list.txt" in the current directory.
