bb319a016a8d867b48e3aa9b14bad5d69c545b1c
Ryan Vergel <ryan.vergel@amd.com>
2025-04-15 16:54:56 -0600

    corrected file list

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="amd-docs-theme",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     package_data={
         'amd_docs_theme': [
