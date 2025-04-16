f059245214bed851273b480e6105b36df39a3040
Ryan Vergel <ryan.vergel@amd.com>
2025-04-16 11:21:21 -0600

    iterated to 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="amd-docs-theme",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     package_data={
         'amd_docs_theme': [
